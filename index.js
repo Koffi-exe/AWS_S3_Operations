fe1b23f4c821ccae8397ed707d2122ee75e8d429
Koffi-exe <adithyaasasi@gmail.com>
2025-05-08 20:57:41 +0530

    operations added: 1.Listing all the s3 buckets, 2. Presigned url to access the s3 content

---
+++ index.js
@@ -0,0 +1,28 @@
+import { S3Client, GetObjectCommand } from "@aws-sdk/client-s3";
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
+import 'dotenv/config';
+
+const s3Client = new S3Client({
+    region: "us-east-1",
+    credentials: {
+        accessKeyId: process.env.accessKeyId,
+        secretAccessKey: process.env.secretAccessKey,
+    },
+})
+
+async function getObjectUrl(key){
+    const command = new GetObjectCommand({
+        Bucket: 'nodeserver-test',
+        Key: key,
+    });
+    try {
+        const url = await getSignedUrl(s3Client, command, { expiresIn: 3600 });
+        console.log('tsi:'+ url);
+        return url;
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+
+getObjectUrl('login_bg.jpg')
---
+++ s3_list_buckets.js
@@ -0,0 +1,45 @@
+import { ListBucketsCommand, paginateListBuckets, S3Client } from "@aws-sdk/client-s3";
+import 'dotenv/config'; 
+
+const s3Client = new S3Client({
+    region: "us-east-1",
+    credentials: {
+        accessKeyId: process.env.accessKeyId,
+        secretAccessKey: process.env.secretAccessKey
+    },
+}); 
+
+const buckets = [];
+var owner = null;
+// Dont panic just look through the AWS documentation for the S3Client and ListBucketsCommand to understand what is going on here.
+const main = async () => {
+    try {
+        const paginator = paginateListBuckets({client: s3Client}, {});
+        for await (const page of paginator) {
+            console.log("Page:", page);
+            if(!owner){
+                owner = page.Owner.DisplayName;
+            }
+            buckets.push(page.Buckets);
+        }
+        console.log("Buckets:", buckets);
+        console.log("Owner:", owner);
+    } catch (error) {
+        console.error("Error listing buckets:", error.message);
+    }
+}
+
+main();
+
+// Uncomment the following code to list buckets without pagination
+
+// const listBuckets = async () => {
+//     try {
+//         const command = new ListBucketsCommand({});
+//         const response = await s3Client.send(command);
+//         console.log("Buckets:", response.Buckets);  
+//     } catch (error) {
+//         console.error("Error listing buckets:", error);
+//     }
+// }
+// listBuckets();
